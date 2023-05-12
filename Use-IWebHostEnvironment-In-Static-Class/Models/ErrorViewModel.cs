51a127163f5f25a617e422eea50e67dc2249e174
mohammad2559 <mohammad.abasy.79@gmail.com>
2023-05-12 07:48:39 -0700

    Add project files.

---
+++ Use-IWebHostEnvironment-In-Static-Class/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Use_IWebHostEnvironment_In_Static_Class.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Use-IWebHostEnvironment-In-Static-Class/Models/StaticClass.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Use_IWebHostEnvironment_In_Static_Class.Models
+{
+    public static class StaticClass
+    {
+        public static IWebHostEnvironment WebEnv()
+        {
+            var _accessor = new HttpContextAccessor();
+            return _accessor.HttpContext.RequestServices.GetRequiredService<IWebHostEnvironment>();
+        }
+    }
+}
